2f8a7e6e0533af9b4b375b74fd1267fdc6d99aa0
Kerollmops <clement@meilisearch.com>
2022-11-09 15:18:50 +0100

    Fix the bindgen bindings

--- lmdb-sys/bindgen.rs
+++ lmdb-sys/bindgen.rs
@@ -10,32 +10,32 @@ impl ParseCallbacks for Callbacks {
     fn int_macro(&self, name: &str, _value: i64) -> Option<IntKind> {
         match name {
             "MDB_SUCCESS"
-            | "MDB_KEYEXIST"
-            | "MDB_NOTFOUND"
-            | "MDB_PAGE_NOTFOUND"
-            | "MDB_CORRUPTED"
-            | "MDB_PANIC"
-            | "MDB_VERSION_MISMATCH"
-            | "MDB_INVALID"
-            | "MDB_MAP_FULL"
-            | "MDB_DBS_FULL"
-            | "MDB_READERS_FULL"
-            | "MDB_TLS_FULL"
-            | "MDB_TXN_FULL"
-            | "MDB_CURSOR_FULL"
-            | "MDB_PAGE_FULL"
-            | "MDB_MAP_RESIZED"
-            | "MDB_INCOMPATIBLE"
+            | "MDB_BAD_DBI"
             | "MDB_BAD_RSLOT"
             | "MDB_BAD_TXN"
             | "MDB_BAD_VALSIZE"
-            | "MDB_BAD_DBI"
-            | "MDB_PROBLEM"
-            | "MDB_LAST_ERRCODE"
-            | "MDB_BAD_CHECKSUM"
+            | "MDB_CORRUPTED"
             | "MDB_CRYPTO_FAIL"
-            | "MDB_ENV_ENCRYPTION" => Some(IntKind::Int),
-            "MDB_SIZE_MAX" | "MDB_PROBLEM" => Some(IntKind::ULongLong),
+            | "MDB_CURSOR_FULL"
+            | "MDB_DBS_FULL"
+            | "MDB_ENV_ENCRYPTION"
+            | "MDB_INCOMPATIBLE"
+            | "MDB_INVALID"
+            | "MDB_KEYEXIST"
+            | "MDB_LAST_ERRCODE"
+            | "MDB_MAP_FULL"
+            | "MDB_MAP_RESIZED"
+            | "MDB_NOTFOUND"
+            | "MDB_PAGE_FULL"
+            | "MDB_PAGE_NOTFOUND"
+            | "MDB_PANIC"
+            | "MDB_PROBLEM"
+            | "MDB_READERS_FULL"
+            | "MDB_TLS_FULL"
+            | "MDB_TXN_FULL"
+            | "MDB_VERSION_MISMATCH"
+            | "MDB_BAD_CHECKSUM" => Some(IntKind::Int),
+            "MDB_SIZE_MAX" => Some(IntKind::ULongLong),
             _ => Some(IntKind::UInt),
         }
     }
@@ -52,15 +52,15 @@ pub fn generate() {
 
     let bindings = bindgen::Builder::default()
         .header(lmdb.join("lmdb.h").to_string_lossy())
-        .whitelist_var("^(MDB|mdb)_.*")
-        .whitelist_type("^(MDB|mdb)_.*")
-        .whitelist_function("^(MDB|mdb)_.*")
+        .allowlist_var("^(MDB|mdb)_.*")
+        .allowlist_type("^(MDB|mdb)_.*")
+        .allowlist_function("^(MDB|mdb)_.*")
         .size_t_is_usize(true)
         .ctypes_prefix("::libc")
-        .blacklist_item("mode_t")
-        .blacklist_item("mdb_mode_t")
-        .blacklist_item("mdb_filehandle_t")
-        .blacklist_item("^__.*")
+        .blocklist_item("mode_t")
+        .blocklist_item("mdb_mode_t")
+        .blocklist_item("mdb_filehandle_t")
+        .blocklist_item("^__.*")
         .parse_callbacks(Box::new(Callbacks {}))
         .layout_tests(false)
         .prepend_enum_name(false)
